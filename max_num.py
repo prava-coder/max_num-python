f6f8f65e2b16748a9c0d30a123a16a2600da8846
prava-coder <96050882+prava-coder@users.noreply.github.com>
2022-02-15 10:18:38 +0530

    Add files via upload

---
+++ max_num.py
@@ -0,0 +1,8 @@
+#write a program to print maximum number
+
+num1=float(input("eneter the first number:"))
+num2=float(input("enter the second number:"))
+if num1>num2:
+  print(num1)
+else:
+  print(num2)
